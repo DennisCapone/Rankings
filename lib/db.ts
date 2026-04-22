37840d840238c9f739e5e1eb3b2bc51ab8b4dc05
DennisCapone <denniscapone.2008@gmail.com>
2026-04-22 12:09:59 +0200

    Refactor global prisma client declaration

--- lib/db.ts
+++ lib/db.ts
@@ -3,20 +3,11 @@ import { PrismaPg } from "@prisma/adapter-pg";
 import { Pool } from "pg";
 
 const connectionString = process.env.DATABASE_URL;
-
 const prismaClientSingleton = () => {
-  // Prisma 7 richiede l'istanza di un pool per connettersi
   const pool = new Pool({ connectionString });
   const adapter = new PrismaPg(pool);
-  
-  // Passiamo l'adapter al client
   return new PrismaClient({ adapter });
 };
-
-declare const globalThis: {
-  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
-} & typeof global;
-
+declare const globalThis: { prismaGlobal: ReturnType<typeof prismaClientSingleton>;} & typeof global;
 export const db = globalThis.prismaGlobal ?? prismaClientSingleton();
-
 if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = db;
