3b6dd8a25e063c0e4b4c9c105702cfddd4f2bb0b
DennisCapone <denniscapone.2008@gmail.com>
2026-04-22 12:45:02 +0200

    Validate input length in addRanking function
    Add input validation to addRanking function.

--- app/actions.ts
+++ app/actions.ts
@@ -8,6 +8,7 @@ export async function handleClick(code: string) {
 }
 export async function addRanking(items:string[], name:string) {
   await db.ranking.create({
+    if (length < 2) {throw new Error("Invalid input")}
     let code = ""
     for (let ind = 0; ind < 8; ind++) code += String.fromCharCode(Math.floor(Math.random() * (90 - 65 + 1) + 65))
     data: { name: name, creator: "Dennis Capone", code: code, items: {
