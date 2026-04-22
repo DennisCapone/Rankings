72e01ede9fdaedaca5b90bf0cf6b264f6082ed40
DennisCapone <denniscapone.2008@gmail.com>
2026-04-22 23:44:24 +0200

    Refactor Create component structure and imports

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -1,26 +1,34 @@
 "use client";
-import Button from '@/components/Button';
+import Button from '../../components/Button';
 import Link from 'next/link';
 import { useState } from 'react';
 import { addRanking } from "../actions";
 import { useRouter } from 'next/navigation';
 
 export default function Create() {
-  const [items, setItems] = useState(['', ''])
-  const [name, setName] = useState('')
   const router = useRouter()
-  const add = () => {if (items[items.length-1] !== "" && items[0] !== "") setItems([...items, ""])}        // Adding element to the array //
-  const remove = (index:number) => {if (items.length > 2)setItems(items.filter((_, i) => i !== index))}    // Remove element from the array //
-  const handleNameChange = (value:string) =>{setName(value)}                                               // Handle the change of the name //
-  const handleChange = (value:string, index:number) => {                                                   // Handle the change of the elements //
+
+  const [items, setItems] = useState(['', ''])
+  const [name, setName] = useState("")
+
+  const add = () => {if (items[items.length-1] !== "" && items[0] !== "") setItems([...items, ""])}
+  const remove = (index:number) => {if (items.length > 2)setItems(items.filter((_, i) => i !== index))}
+
+  const handleNameChange = (value:string) =>{setName(value)}
+  const handleChange = (value:string, index:number) => {
     const newItems = [...items]
     newItems[index] = value
     setItems(newItems)
-    if (value == "") remove(index)}
-  const Save = async () => {                                                      // Save the ranking in the database
-    const code = await addRanking(items, name)                                    // and go to the share link page
-    router.push(`/share/${code}`)}
+    if (value == "") remove(index)
+  }
   
+  const Save = async () => {
+    const code = await addRanking(items, name)
+    router.push(`/share/${code}`)
+  }
+  
+
+
   return (
     <>
       <div className='flex justify-center mt-30'><input
@@ -29,19 +37,25 @@ export default function Create() {
         placeholder = "Nome"
         className = 'mr-3 border-black border-solid border-2 h-14 text-xl outline-none rounded-xl'
       /></div>
+
       <div className='flex justify-center mt-5'><h1 className='mb-5 text-3xl'> Lista: </h1></div>
-      {items.map((item, i) => (
-        <div key={i} className='mb-3 ml-3 flex justify-center'>
-          <input
-            type="text"
-            value = {item}
-            onChange = {e => (handleChange(e.target.value, i))}
-            placeholder = {`Elemento ${i+1}`}
-            className = 'mr-3 border-black border-solid border-2 h-14 text-xl outline-none rounded-xl'/>
-          <button onClick={() => {remove(i)}} className='cursor-pointer border-black border-solid border-2 h-14 w-14 text-xl rounded-2xl'> X </button>
-          </div>))}
+      {
+        items.map((item, i) => (
+          <div key={i} className='mb-3 ml-3 flex justify-center'>
+            <input
+              type="text"
+              value = {item}
+              onChange = {e => (handleChange(e.target.value, i))}
+              placeholder = {`Elemento ${i+1}`}
+              className = 'mr-3 border-black border-solid border-2 h-14 text-xl outline-none rounded-xl'
+            />
+            <button onClick={() => {remove(i)}} className='cursor-pointer border-black border-solid border-2 h-14 w-14 text-xl rounded-2xl'> X </button>
+          </div>
+        ))}
+
       <div className='flex justify-center'><button onClick={() => add()} className='cursor-pointer rounded-2xl h-14 text-xl border-black border-solid border-2 w-35'> AGGIUNGI </button></div>
       <div className='flex justify-center'><button onClick={Save} className='flex justify-center mt-30'><Button text="CREA" color='bg-blue-700' textcolor='text-white' bcolor='' /></button></div>
+      
       <Link href="/"><div className='flex justify-center mt-3'><Button text="Torna indietro" color="bg-red-300" textcolor='' bcolor='' /></div></Link>
     </>
   )
