5853cac7055865d9b68a700142b3bb54e9d3cc1f
DennisCapone <denniscapone.2008@gmail.com>
2026-04-22 12:07:05 +0200

    Refactor addRanking function and simplify queries

--- app/actions.tsx
+++ app/actions.tsx
@@ -3,27 +3,13 @@ import { db } from "@/lib/db";
 
 export async function handleClick(code: string) {
   const exist = await db.ranking.findUnique({
-    where: { code: code }
-  });
+    where: { code: code }});
   return !!exist; 
 }
 
 export async function addRanking(code:string, items:string[], name:string) {
   await db.ranking.create({
-    data: {
-      name: name,
-      creator: "Dennis Capone",
-      code: code,
-      items: {
-        create: items.map(n => ({
-          name: n
-        }))
-        }
-      }
-    }
-  )
-  console.log("ho salvato!")
-
-
+    data: { name: name, creator: "Dennis Capone", code: code, items: {
+      create: items.map(n => ({name: n}))}}})
   return null
 }
