88e3d12c5c1df58738cd8742dcde02385e43ff42
DennisCapone <denniscapone.2008@gmail.com>
2026-04-23 00:29:54 +0200

    Fix code generation logic in addRanking function

--- app/actions.tsx
+++ app/actions.tsx
@@ -7,7 +7,7 @@ export async function checkCode(code: string) {
 
 export async function addRanking(items:string[], name:string) {
   let code = ""
-    for (let ind = 0; ind < 8; ind++) code += String.fromCharCode(Math.floor(Math.random() * (90 - 65 + 1) + 65))    // make the code and create a ranking with
+    for (let ind = 0; ind < 8; ind++) code += String.fromCharCode(Math.floor(Math.random() * (90 - 65 + 1) + 65))    // make the code and create a ranking with the items in input
   await db.ranking.create({
     data: { name: name, creator: "Dennis Capone", code: code, elements: items.length, items: {                       // items in input
       create: items.map(n => ({name: n}))}}})
