b078c66c54102f73e2963e8fd034acef159431d2
DennisCapone <denniscapone.2008@gmail.com>
2026-04-22 15:53:17 +0200

    Remove unnecessary fragment in Menu component

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,7 @@ import Link from "next/link"
 
 export default function Menu() {
   return (
-    <>
-      <div className="flex flex-col h-screen justify-center">
-        <Link href="/create"><div className='flex justify-center'><Button color="bg-blue-600" text="CREA" textcolor="text-white" bcolor="border-white" /></div></Link>
-        <Link href="/play"><div className='flex justify-center'><Button color="bg-white" text="GIOCA" textcolor="text-black" bcolor="border-black" /></div></Link>
-      </div>
-    </>
-  )
-}
+    <div className="flex flex-col h-screen justify-center">
+    <Link href="/create"><div className='flex justify-center'><Button color="bg-blue-600" text="CREA" textcolor="text-white" bcolor="border-white" /></div></Link>
+    <Link href="/play"><div className='flex justify-center'><Button color="bg-white" text="GIOCA" textcolor="text-black" bcolor="border-black" /></div></Link>
+    </div>)}
