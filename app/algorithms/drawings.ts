e4cdd9b4804f5513cc3dba0d0d15cb4c1ff535a0
DennisCapone <denniscapone.2008@gmail.com>
2026-04-22 21:56:47 +0200

    Fix ranking retrieval and probability calculation

--- app/algorithms/drawings.ts
+++ app/algorithms/drawings.ts
@@ -3,15 +3,15 @@ import { db } from "@/lib/db";
 
 export async function drawingNormal({params} : {params: Promise<{code:string}>}) {
   const { code } = await params                                                                // Take the code from the url //
-  const ranking = await db.ranking.findUnique({                                                /** Take the ranking's items
-    where: {code: code,},                                                                        from the database */
+  const ranking = await db.ranking.findUnique({                                                // Take the ranking's items
+    where: {code: code,},                                                                      //  from the database
     include: {items: {orderBy: { points: "desc" }}}})
   let chosen = [], total = 0, avaibleItems = [...ranking.items]
   for (const item of ranking.items) total += item.probability                                  // Algorithm to make a weighted extraction //
   for (let i=0;i<2;i++) {
     let random = Math.random() * total, last = 0
     for (const item of ranking.items) {
-      if ((i === 0 || chosen[0].id !== item.id) && random <= last += item.probability) {
+      if ((i === 0 || chosen[0].id !== item.id) && random <= (last += item.probability)) {
         chosen[i] = item
         break}}
     if (!chosen[i]) {chosen[i] = ranking.items[ranking.items.length - 1];}
