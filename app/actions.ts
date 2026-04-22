d8f8df3917780f025ac4e9a5e398e13d6182deb4
DennisCapone <denniscapone.2008@gmail.com>
2026-04-22 21:42:57 +0200

    Refactor checkCode and addRanking functions

--- app/actions.ts
+++ app/actions.ts
@@ -2,15 +2,14 @@
 import { db } from "@/lib/db";
 
 export async function checkCode(code: string) {
-  const exist = await db.ranking.findUnique({where: { code: code }})        /** Search the code in the database
-  return !!exist}                                                           and return if he found it */
+  const exist = await db.ranking.findUnique({where: { code: code }})        // Search the code in the database
+  return !!exist}                                                           // and return if he found it
 
 export async function addRanking(items:string[], name:string) {
+  let code = ""
+    for (let ind = 0; ind < 8; ind++) code += String.fromCharCode(Math.floor(Math.random() * (90 - 65 + 1) + 65))    // make the code and create a ranking with
   await db.ranking.create({
-    if (items.length < 2) {throw new Error("Invalid input")}                // Check error //
-    let code = ""
-    for (let ind = 0; ind < 8; ind++) code += String.fromCharCode(Math.floor(Math.random() * (90 - 65 + 1) + 65))    /** make the code and create a ranking with
-    data: { name: name, creator: "Dennis Capone", code: code, elements: items.length, items: {                        items in input */
+    data: { name: name, creator: "Dennis Capone", code: code, elements: items.length, items: {                       // items in input
       create: items.map(n => ({name: n}))}}})
   return code
 }
