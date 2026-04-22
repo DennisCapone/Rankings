dca1a8ffa51549939b5cbd3512a348576c608e1f
DennisCapone <denniscapone.2008@gmail.com>
2026-04-22 12:09:17 +0200

    Fix button component JSX structure

--- components/Button.tsx
+++ components/Button.tsx
@@ -2,8 +2,7 @@ export default function Button({color, text, textcolor, bcolor} : {color:string;
   return (
     <>
       <div className={`h-20 w-40 ${color} cursor-pointer rounded-3xl flex justify-center items-center ${bcolor} border-solid border-3 mb-1`}>
-        <h1 className={`${textcolor} text-xl`}> {`${text}`} </h1>
-      </div>
+      <h1 className={`${textcolor} text-xl`}> {`${text}`} </h1></div>
     </>
   )
 }
