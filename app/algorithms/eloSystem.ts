39546e09f6f199035617131a6e7a8dc3dbf691d7
DennisCapone <denniscapone.2008@gmail.com>
2026-04-23 13:25:40 +0200

    Change id parameters to bigint in eloSystem function

--- app/algorithms/eloSystem.ts
+++ app/algorithms/eloSystem.ts
@@ -1,7 +1,7 @@
 "use server"
 import { db } from "@/lib/db";
 
-export async function eloSystem( idA: number, idB: number, K: number, aWinned: boolean) {
+export async function eloSystem( idA: bigint, idB: bigint, K: number, aWinned: boolean) {
     const itemA = await db.item.findUnique({where: { id: idA }, select: { points: true }});
     const itemB = await db.item.findUnique({where: { id: idB }, select: { points: true }});
     if (!itemA || !itemB) throw new Error("One or both items not found")
