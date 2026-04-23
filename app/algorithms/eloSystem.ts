48363b927af5261b198be5325397b70bc19f9bf4
DennisCapone <denniscapone.2008@gmail.com>
2026-04-23 14:07:04 +0200

    Update eloSystem.ts

--- app/algorithms/eloSystem.ts
+++ app/algorithms/eloSystem.ts
@@ -6,9 +6,7 @@ export async function eloSystem( idA: bigint, idB: bigint, K: number, aWinned: b
     const itemB = await db.item.findUnique({where: { id: idB }, select: { points: true }});
     if (!itemA || !itemB) throw new Error("One or both items not found")
     const expA = 1 / (1 + Math.pow(10, (itemB.points-itemA.points)/400))
-    const aInc = aWinned ? K*(1-expA) : -K*(expA)
-    await Promise.all([
+    const aInc = Math.round(aWinned ? K*(1-expA) : -K*(expA))
     await db.$transaction([
-        db.$executeRaw`UPDATE "items" SET points = points + ${Math.round(aInc)} WHERE id = ${idA}`,
-        db.$executeRaw`UPDATE "items" SET points = points - ${Math.round(aInc)} WHERE id = ${idB}`])])
-}
+        db.item.update({ where: { id: idA }, data: { points: { increment: aInc }}}),
+        db.item.update({ where: { id: idB }, data: { points: { increment: -aInc }}})])
