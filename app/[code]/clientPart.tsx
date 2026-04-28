30bbcde6b72c203de51c4ec378b29e8734eb0d1e
DennisCapone <denniscapone.2008@gmail.com>
2026-04-28 08:07:09 +0000

    try

--- app/[code]/clientPart.tsx
+++ app/[code]/clientPart.tsx
@@ -44,6 +44,7 @@ export default function ClientPart({ code, initialPlayers, numPairs }: { code: s
     if (exctracted < numPairs-10) {
       fillQueue()
     }
+    console.log(pairs)
     setExctracted(exctracted+1)
     eloSystem(code, vote, currentPair?.p1.id || 0, currentPair?.p2.id || 0)
     setCurrentPair(pairs[0])
