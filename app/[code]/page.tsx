259924b06d729a1200df9a8fe1a03eda1b2a539f
DennisCapone <denniscapone.2008@gmail.com>
2026-04-28 08:39:01 +0000

    Reorder drawing function call to ensure Redis reset occurs first

--- app/[code]/page.tsx
+++ app/[code]/page.tsx
@@ -5,13 +5,15 @@ import { fast_db } from "@/lib/fast_db";
 export default async function Play({ params }: { params: Promise<{ code: string }> }) {
   // Call all the client part from another component to be able to prefetche data and avoid loading times //
   const { code } = await params;
-  const result = await drawing(code)
-  if (!result) return null
-  const [ initialPlayers ] = result
 
   // Reset the Redis at the open of the page //
   await fast_db.del(`queue:${code}`)
   await fast_db.del(`drawn_pairs:${code}`)
+
+  
+  const result = await drawing(code)
+  if (!result) return null
+  const [ initialPlayers ] = result
 
   // Defining the number of the pairs //
   const itemsLength = await fast_db.hlen(`ranking:${code}`)
